05671881f652d0ba27a5b9dc252f4c4756b1507a
yal <643589668@qq.com>
2022-10-26 17:27:40 +0800

    为什么不使用接口而使用类来定义DTO？

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,5 +1,11 @@
 import { IsString } from "class-validator";
 //这个包会帮助我们校验，不通过会返回报错信息和状态码
+//为什么不定义接口，而定义类？虽然都可以对类型进行约束
+/**
+ * 类是 JavaScript ES6 标准的一部分，因此它们在编译后的 JavaScript 中被保留为实际实体。
+ * 另一方面，由于 TypeScript 接口在转换过程中被删除，所以 Nest 不能在运行时引用它们。
+ * 这一点很重要，因为诸如管道（Pipe）之类的特性为在运行时访问变量的元类型提供更多的可能性。
+ */
 export class CreateCoffeeDto {
   @IsString()
   readonly name: string;
